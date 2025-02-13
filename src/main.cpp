85452f518eae0a4abf9508edf87f7ac184bd16c8
urabexon <hiroki708.a48uk@gmail.com>
2025-02-14 01:27:55 +0900

    test fix

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,18 @@
 /*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 00:14:07 by hirokiurabe       #+#    #+#             */
-/*   Updated: 2025/02/14 00:14:14 by hirokiurabe      ###   ########.fr       */
+/*   Updated: 2025/02/14 01:25:22 by hirokiurabe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+// Sample(必要になったものをインクルード)
+// #include "atcoder/brute_force.hpp"
+
 int main() {
-    // ここで各アルゴリズムのテストや利用例を記述してください。
+    // std::cout << "Library Test\n";
+    // int n = 3;
+    // std::cout << "n = " << n << " の全部分集合:\n";
+    // bruteForceExample(n);
     return 0;
 }
---
+++ tests/test_main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_main.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hirokiurabe <hirokiurabe@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 01:23:50 by hirokiurabe       #+#    #+#             */
+/*   Updated: 2025/02/14 01:26:06 by hirokiurabe      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cassert>
+#include <vector>
+// Sample
+// #include "atcoder/cumulative_sum.hpp"
+
+void testPrefixSum() {
+    // std::vector<long long> a = {1, 2, 3, 4};
+    // auto ps = prefixSum(a);
+    // assert(ps[0] == 0);
+    // assert(ps[1] == 1);
+    // assert(ps[2] == 3);
+    // assert(ps[3] == 6);
+    // assert(ps[4] == 10);
+}
+
+int main() {
+    // testPrefixSum();
+    return 0;
+}
